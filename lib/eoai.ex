1c8fa6bf4cfd8c3926e126a4638b7355ca6a6adf
Yoshioka <sss.yoshioka@gmail.com>
2023-04-14 13:17:27 +0000

    initial commit

---
+++ lib/eoai.ex
@@ -0,0 +1,18 @@
+defmodule Eoai do
+  @moduledoc """
+  Documentation for `Eoai`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Eoai.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/eoai_test.exs
@@ -0,0 +1,8 @@
+defmodule EoaiTest do
+  use ExUnit.Case
+  doctest Eoai
+
+  test "greets the world" do
+    assert Eoai.hello() == :world
+  end
+end
