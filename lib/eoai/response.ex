7d6bc0cbbc5ac39bbbb1551affd7b0a50b83b5bb
Yoshioka <sss.yoshioka@gmail.com>
2023-05-05 01:57:06 +0000

    🎸 feat: response format error

--- lib/eoai/response.ex
+++ lib/eoai/response.ex
@@ -1,15 +1,42 @@
 defmodule Eoai.Response do
+  require Logger
+
   def dig(response, keys) when is_list(keys) do
+    Enum.reduce_while(keys, response.body, fn key, json ->
+      case do_dig(key, json) do
+        {:ok, result} ->
+          {:cont, result}
+
+        _ ->
+          Logger.error("Response format does not match.")
+          IO.inspect(response.body)
+          {:halt, nil}
+      end
+    end)
+  end
+
+  def dig!(response, keys) when is_list(keys) do
     Enum.reduce(keys, response.body, fn key, json ->
-      do_dig(key, json)
+      case do_dig(key, json) do
+        {:ok, result} ->
+          result
+
+        _ ->
+          IO.inspect(response.body)
+          raise "Response format does not match."
+      end
     end)
   end
 
   defp do_dig(key, json) when is_map(json) do
-    Map.get(json, key)
+    {:ok, Map.get(json, key)}
   end
 
   defp do_dig(key, json) when is_list(json) do
-    Enum.at(json, key)
+    {:ok, Enum.at(json, key)}
+  end
+
+  defp do_dig(_key, _json) do
+    :error
   end
 end
